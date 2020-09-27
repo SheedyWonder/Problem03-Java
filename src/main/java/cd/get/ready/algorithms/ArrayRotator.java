8896d83419c39d046f9a23ce40b9b06bf669d9c0
SheedyWonder <rasheed.saka85@yahoo.com>
2020-09-27 03:29:48 -0400

    Program 3

--- src/main/java/cd/get/ready/algorithms/ArrayRotator.java
+++ src/main/java/cd/get/ready/algorithms/ArrayRotator.java
@@ -1,10 +1,29 @@
 package cd.get.ready.algorithms;
-
+import java.util.*;
 public class ArrayRotator {
 
-    public int[] rotateLeft(int[] arr, int n){
+    public int[] rotateLeft(int[] arr, int shift)
+    {
+        int size = arr.length;
+        int[] temp = new int[size];
 
-        return arr;
+        int i = 0;
+        int rotate_i = shift;
+        while(rotate_i < size)
+        {
+            temp[i] = arr[i];
+            i++;
+            rotate_i++;
+        }
+
+        rotate_i = 0;
+        while (rotate_i < shift)
+        {
+            temp[i] = arr[i];
+            i++;
+            rotate_i++;
+        }
+        return temp;
     }
 
 
